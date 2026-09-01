19acb784d6020f146c1e5667b4c5043e311d9dd3
Jeffrey Wilbur <jeff@strutb.com>
2026-09-01 17:01:56 -0600

    0x10008A90 M8A90 byte-exact: nested self-vcall pair v7(v3(a,b))

---
+++ src/core/cpp/0x10008A90.cpp
@@ -0,0 +1,27 @@
+/* @implements 0x10008A90 glide M8A90
+ * @cpp_kind method
+ * @cpp_symbol ?M8A90@Vt8A90@@QAEHHH@Z
+ *
+ * Twin of 0x10008A70: return v7(v3(a, b)); vtbl cached in edi.
+ */
+#ifdef BR_MATCHING_BUILD
+#define _CRTIMP __declspec(dllimport)
+#endif
+
+class Vt8A90 {
+public:
+    virtual void v0();
+    virtual void v1();
+    virtual void v2();
+    virtual int v3(int, int);
+    virtual void v4();
+    virtual void v5();
+    virtual void v6();
+    virtual int v7(int);
+    int M8A90(int a, int b);
+};
+
+int Vt8A90::M8A90(int a, int b)
+{
+    return v7(v3(a, b));
+}
