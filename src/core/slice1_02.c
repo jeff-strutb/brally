06413e816461c3d60d6266287bd600881ac096b5
Jeffrey Wilbur <jeff@strutb.com>
2026-09-03 22:35:47 -0600

    refile startup: move the message one-shot release out of slice1_02.c

--- src/core/slice1_02.c
+++ src/core/slice1_02.c
@@ -527,31 +527,9 @@ int BrNetReset(BrNetState *pNet)
 }
 
 
-extern int FUN_1006ba60(int a, int b);
-extern unsigned char *DAT_104abb20;
-extern int DAT_104abb24;
+/* 0x10006460 FUN_10006460 now lives in
+ * src/core/startup/br_msgoneshot.c. */
 
-/* WHAT IT DOES: release the pending input one-shot under the message mutex
- * -- silences its sound, frees the slot, and restores the default message
- * table and level if one was armed. */
-/* @implements 0x10006460 glide FUN_10006460 */
-/* Mutex-guarded release of one input one-shot: stop the pending sound and
- * reset its slot, and if the arm flag is set restore the default table
- * pointer (&DAT_1021c9b0) and its 3.0f level.  Same lock idiom as BrNetReset. */
-void FUN_10006460(void)
-{
-    WaitForSingleObject((void *)DAT_10226a54, 0xffffffff);
-    if (DAT_10226a28 >= 0) {
-        FUN_1006ba60(DAT_10226a28, 0x200020);
-        DAT_10226a28 = -1;
-    }
-    if (DAT_10226a38 != 0) {
-        DAT_104abb20 = &DAT_1021c9b0;
-        DAT_104abb24 = 0x40400000;
-        DAT_10226a38 = 0;
-    }
-    ReleaseMutex((void *)DAT_10226a54);
-}
 #else
 int BrNetReset(BrNetState *pNet)
 {
