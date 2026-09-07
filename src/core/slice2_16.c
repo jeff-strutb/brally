4b1c0c368c4b6f073fbdfeae9f218fd8b157f0df
Jeffrey Wilbur <jeff@strutb.com>
2026-09-07 14:41:05 -0600

    0x10029420 BrGbiTexScanSetImg: @t4-pass ledger line (tools/crank.py, 53 compiles, best reg 0 insn +0 regions 2 bytes +0 raw 45)

--- src/core/slice2_16.c
+++ src/core/slice2_16.c
@@ -671,6 +671,7 @@ void BrGbiTexScanTexture(BrGbiTexScan *pSt, const BrGfxWords *pCmd)
  * size of the image a load is about to read from, and -- if a run was not
  * already in progress -- marks this command as where the run begins. */
 /* @implements 0x10029EB0 d3d BrGbiTexScanSetImg */
+/* @t4-pass 0x10029420 1 2026-09-07 probes 53 bytes 83 insns 24 regions 2 rows 0 census yes  (tools/crank.py) */
 /* @implements 0x10029420 glide BrGbiTexScanSetImg */
 #ifdef BR_MATCHING_BUILD
 void BrGbiTexScanSetImg(BrGfxWords *pCmd)
